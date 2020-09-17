9544c6e967b91986aa11913040691b094936bb85
djddanman <djddanman@gmail.com>
2020-09-16 19:40:23 -0500

    Change lambdas to normal functions Fix warnings

--- main.py
+++ main.py
@@ -11,36 +11,47 @@ items_dict = dict()
 
 
 def calculate_stack_lambda(base_value, first_item, stack_effect, stack_type, limit=None):
+    effect = None
     if not limit:
         limit = [0, float('inf')]
 
     if stack_type == 'linear':
-        effect = lambda i: (np.array(i) < limit[1]) * (
-                base_value + (np.array(i) > 0) * first_item + (np.array(i) > 1) * (
-                stack_effect * (i - 1))) + (np.array(i) >= limit[1]) * limit[0]
+        def effect(i):
+            (np.array(i) < limit[1]) * (
+                    base_value + (np.array(i) > 0) * first_item + (np.array(i) > 1) * (stack_effect * (i - 1))) + (
+                    np.array(i) >= limit[1]) * limit[0]
 
     elif stack_type == 'hyperbolic':
-        effect = lambda n: 1 - 1 / (stack_effect * n + 1)
+        def effect(n):
+            1 - 1 / (stack_effect * n + 1)
 
     elif stack_type == 'exponential':
-        effect = lambda n: 1 - (1 - stack_effect) ** n
+        def effect(n):
+            1 - (1 - stack_effect) ** n
 
     elif stack_type == 'bandolier':
-        effect = lambda n: 1 - 1 / (1 + n) ** 0.33
+        def effect(n):
+            1 - 1 / (1 + n) ** 0.33
 
     elif stack_type == 'key':
-        common_rarity = lambda n: 80 / (80 + 20 * n + n ** 2) * 100
-        uncommon_rarity = lambda n: n * 20 / (80 + 20 * n + n ** 2) * 100
-        legendary_rarity = lambda n: n ** 2 / (80 + 20 * n + n ** 2) * 100
+        def common_rarity(n):
+            80 / (80 + 20 * n + n ** 2) * 100
+
+        def uncommon_rarity(n):
+            n * 20 / (80 + 20 * n + n ** 2) * 100
+
+        def legendary_rarity(n):
+            n ** 2 / (80 + 20 * n + n ** 2) * 100
+
         effect = [common_rarity, uncommon_rarity, legendary_rarity]
 
     elif stack_type == 'genesis':
-        effect = lambda n: base_value / (
-                1 - (np.array(n) > 0) * (-1) + (np.array(n) > 0) * first_item + (np.array(n) > 1) * (
-                stack_effect * (n - 1)))
+        def effect(n):
+            base_value / (1 - (np.array(n) > 0) * (-1) + (np.array(n) > 0) * first_item + (np.array(n) > 1) * (
+                        stack_effect * (n - 1)))
 
     elif stack_type == 'corpsebloom':
-        effect = lambda n: first_item * stack_effect ** (n - 1)
+        def effect(n): first_item * stack_effect ** (n - 1)
 
     return effect
 
@@ -58,7 +69,8 @@ def get_items_dict():
     return data
 
 
-def select_item_gui(items_dict):
+def select_item_gui():
+    global items_dict
     select_item_list = list()
     for rarity in items_dict:
         for item in items_dict[rarity]:
@@ -98,7 +110,7 @@ def select_item_gui(items_dict):
 def item_stacking():
     global items_dict
 
-    item, rarity, effect = select_item_gui(items_dict)
+    item, rarity, effect = select_item_gui()
     item_effect = items_dict[rarity][item][effect]
 
     upper_bound = item_effect['limit'][1] + 2 if 'limit' in item_effect else 26
